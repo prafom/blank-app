4ceb25c0c4f5932c596d7642be091c48b0b4c6e7
prafom <rafayeltgm@gmail.com>
2025-10-21 05:24:38 +0000

    Changed the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("🎈 My new Rafayel app")
+st.title("Rafayel Petrosyan")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )

6dd7664ab33e4fbadd840931d30b308e5c900dbc
prafom <rafayeltgm@gmail.com>
2025-10-21 05:23:43 +0000

    DF and MAP are added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 My new Rafayel app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("Data here:")
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+st.map()
